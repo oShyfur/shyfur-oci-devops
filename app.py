6e1694ffd00b8591827169ab5a2ba6bcd9f01461
oShyfur <md.shyfur.rahman@oracle.com>
2024-10-30 12:47:58 +0600

    app new v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def This_is_My_Demo():
-    return 'Hello, World!'
+    return 'Hello, World v2!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
