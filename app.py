67b6cc70348ffe6303cbbedb85968f05975b229d
Md Shyfur Rahman <106128043+oShyfur@users.noreply.github.com>
2024-10-30 13:21:29 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def This_is_My_Demo():
-    return 'Hello, World v2!'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
